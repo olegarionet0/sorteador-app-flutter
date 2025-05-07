4d76975e6271674a1aeaab0ba3e5172c31f4edd0
Olegário Neto <olegarioifce@gmail.com>
2025-05-07 16:19:17 -0300

    adcionando campos de texto

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:meu_app_flutter_clone/services/sorteador_service.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,21 +11,41 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   var numeroSorteado = 0;
+  var qtdSorteios = 0;
+  var ultimoSorteio = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Meu Primeiro App Flutter"),
+        title: Text("Meu Primeiro App Flutter", style:GoogleFonts.pacifico()),
         backgroundColor: Theme.of(context).colorScheme.primary,
       ),
-      body: Center(child: Text(numeroSorteado.toString())),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "Tivemos até agora $qtdSorteios sorteios!", 
+            style: GoogleFonts.roboto(fontSize: 70) ),
+          Text(
+            numeroSorteado.toString(), 
+            style: GoogleFonts.roboto(fontSize: 100) ),
+          Text(
+            "O último número sorteado foi: $ultimoSorteio",
+            style: GoogleFonts.roboto(fontSize: 50) )
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.monetization_on_outlined),
         onPressed: () {
           setState(() {
+            qtdSorteios++;
             numeroSorteado = SorteadorService.sortearNumero();
+            
+
           });
+          ultimoSorteio = numeroSorteado;
           debugPrint(numeroSorteado.toString());
         },
       ),
